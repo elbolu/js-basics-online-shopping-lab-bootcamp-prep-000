4006a53915e8fe9ca720fce79e2bc41d4868d58a
Leah Hill <leah@verysandwich.com>
2018-08-07 15:21:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,19 +50,18 @@ function generateCartItem(itemName) {
   }
 }
 
-function generateCartDescription() {
+function cartDescription(){
   var cartDescription = 'In your cart, you have '
-  if ( getCart().length >= 1 ) {
+  if (getCart().length >=1) {
     cartDescription += `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`
   }
-  if ( getCart().length >= 2 ) {
-    var middleCartItemsDescription = ''
-    for (var i=1; i<getCart().length -1; i++) {
-      middleCartItemsDescription += `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
+  if (getCart().length >=2) {
+    var cartItemsDescription = ''
+    for (var i=1; i<getCart().length -1; i++){
+      cartItemsDescription += `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
     }
-    cartDescription += `${middleCartItemsDescription}, and ${getCart()[getCart().length -1].itemName} at $${getCart()[getCart().length -1].itemPrice}`
+    cartDescription += `${cartItemsDescription}, and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}`
   }
-
   return `${cartDescription}.`
 }
 
