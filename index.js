7fa1de961c36dfab503ef8a0572c93c1758b8ade
Leah Hill <leah@verysandwich.com>
2018-08-07 15:03:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ function setCart(c) {
 
 function addToCart(item) {
   getCart().push(item)
-  
+  function generateCartItem(itemName) {
+  return {
+    itemName:itemName,
+    itemPrice:getRandomInt(1, 100)
+  }
+}
   return `${item.itemName} has been added to your cart.`
 }
 
