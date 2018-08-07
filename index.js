8908a693d2ac4ae63cd65ecd140490192d2bfa26
Leah Hill <leah@verysandwich.com>
2018-08-07 15:29:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,25 @@ function addToCart(item){
   cart.push(itemAdded);
  return `${item} has been added to your cart.`;
 }
-
+var string = "In your cart, you have ";
+  var itemstolist = [];
+  if (cart[0]===undefined) {
+    return "Your shopping cart is empty.";
+  }
+  else if (cart.length===1)
+    {
+      string+=`${cart[0].itemName} at $${cart[0].itemPrice}.`;
+      return string;
+    }
+    else if (cart.length===2) {
+      string+=`${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+      return string; 
+    }
+    else {
+      for (let i=0; i<cart.length-2; i++) {
+        itemstolist.push(`${cart[i].itemName} at $${cart[i].itemPrice}`);
+      }
+      return `${string}${itemstolist}, ${cart[cart.length-2].itemName} at $${cart[cart.length-2].itemPrice}, and ${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`;
+    }
+    
+    
